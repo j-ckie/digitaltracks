03b8f0f8e7e8a35d555e40f0a1b8c13cd328e8d9
Jaclyn Legacion <jglegacion@gmail.com>
2019-10-03 10:52:23 -0500

    change API key and add random video functionality

--- scripts/jackie-scripts.js
+++ scripts/jackie-scripts.js
@@ -2,6 +2,112 @@
 var apiKey = "AIzaSyBBUotQvCorzLIWF8xQXUI6sZuDzyZOacU";
 var apiKey2 = "AIzaSyCmwi-9Qh215YAYaOcOdjZVdS51T7owlF4";
 
+var movieArray = [
+    "The Good, the Bad and the Ugly",
+    "The Godfather",
+    "DrNo",
+    "For a Few Dollars More",
+    "A Fistful of Dollars",
+    "Werckmeister Harmonies",
+    "Hachi: A Dog's Tale",
+    "Scarface",
+    "The Graduate",
+    "The Big Lebowski",
+    "Wall Street",
+    "Day of Anger",
+    "Reservoir Dogs",
+    "Rain Man",
+    "Once Upon a Time in the West",
+    "Once Upon a Time in America",
+    "The Mission",
+    "Drive",
+    "Twin Peaks",
+    "Vertigo",
+    "The Intouchables",
+    "Django Unchained",
+    "Casino",
+    "Stand by Me",
+    "Pulp Fiction",
+    "Groundhog Day",
+    "The Big Gundown",
+    "In the Heat of the Night",
+    "High Noon",
+    "Forrest Gump",
+    "Companeros",
+    "True Detective",
+    "Meet Joe Black",
+    "Beverly Hills Cop",
+    "Dumb and Dumber",
+    "Titanic",
+    "Top Gun",
+    "Mean Streets",
+    "Requiem for a Dream",
+    "The Exorcist",
+    "Zodiac",
+    "The X-Files",
+    "The Man with the Golden Arm",
+    "Death Rides a Horse",
+    "Lock, Stock and Two Smoking Barrels",
+    "Singin' in the Rain",
+    "The Straight Story",
+    "Hang 'Em High",
+    "Eyes Wide Shut",
+    "Fallen Angels",
+    "Blood Simple",
+    "Hair",
+    "American Beauty",
+    "Ronin",
+    "Kickboxer 2: The Road Back",
+    "Farewell, My Lovely",
+    "Desperado",
+    "Johnny English",
+    "Psycho",
+    "Jackie Brown",
+    "Mamma Mia!",
+    "Easy Rider",
+    "Beverly Hills Cop II",
+    "Hawaii Five-O",
+    "Pirates of the Caribbean: The Curse of the Black Pearl",
+    "Taxi Driver",
+    "Magnolia",
+    "The Last of the Mohicans",
+    "8 Mile",
+    "Harry Potter and the Sorcerer's Stone",
+    "Gladiator",
+    "The Wizard of Oz",
+    "Phantasm",
+    "Midnight Cowboy",
+    "The Magnificent Seven",
+    "The Great Escape",
+    "Manhunter",
+    "The Thomas Crown Affair",
+    "Rocky III",
+    "My Sassy Girl",
+    "Gunfight at the O.KCorral",
+    "A View to a Kill",
+    "Mulholland Drive",
+    "The Third Man",
+    "The Sting",
+    "Elmer Gantry",
+    "Kill Bill: Vol1",
+    "Duck, You Sucker",
+    "Trainspotting",
+    "The Seventh Seal",
+    "Donnie Darko",
+    "The Terminator",
+    "The Color of Money",
+    "The Professional",
+    "Crash",
+    "In the Name of the Father",
+    "O Brother, Where Art Thou?",
+    "This is Spinal Tap",
+    "Interstellar",
+    "Inception",
+    "Arrival",
+    "Dunkirk"
+];
+
+
 $(document).ready(function () {
     $('#search-term').submit(function (event) {
         event.preventDefault();
@@ -45,7 +151,7 @@ function getRequest(searchTerm) {
     var url = 'https://www.googleapis.com/youtube/v3/search';
     var params = {
         part: 'snippet',
-        key: apiKey, 
+        key: apiKey2, 
         q: searchTerm,
         type: 'video',
         maxResults: '1'
@@ -58,7 +164,7 @@ function getVideo(searchTerm) {
     var url = 'https://www.googleapis.com/youtube/v3/videos';
     var params = {
         part: 'player',
-        key: apiKey, // API key - quota for one day is 10,000; resets nightly at 12am PST
+        key: apiKey2, // API key - quota for one day is 10,000; resets nightly at 12am PST
         id: searchTerm,
         "status": {
             "uploadStatus": "processed",
@@ -66,7 +172,7 @@ function getVideo(searchTerm) {
             "license": "youtube",
             "embeddable": true,
             "publicStatsViewable": true
-        },
+        }
     };
   
     $.getJSON(url, params, showVideo);
@@ -91,7 +197,7 @@ function showVideo(results) {
     }); 
     
     $('#search-results').html(html);
-    hideSearch()
+    hideSearch();
 }
 
 function hideSearch() {
@@ -111,3 +217,53 @@ function showResults(results) {
 function refreshPage() {
     location.reload();
 }
+
+
+
+// stuff to shuffle movie array
+function shuffleMovie(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+// random movie button logic
+$(document).ready(function () {
+    $('#movieRandom').click(function (event) {
+        event.preventDefault();
+        shuffleMovie(movieArray);
+        var chosenMovie = movieArray.pop();
+        getRequest(chosenMovie.toString() + 'soundtrack');
+        var randomMovieURL = "https://www.omdbapi.com/?apikey=dcf79638&t=" + chosenMovie;
+        var randomMovieOptions = {
+            s: "",
+        }
+
+        function displayRandomMovies(data) {
+            console.log(data);
+            console.log(data.Title);
+            console.log(data.Genre);
+            console.log(data.Runtime);
+            console.log(data.Plot);
+            var randomMoviePoster = '<div>';
+            var randomMovieHTML = '<ul>';
+            randomMoviePoster += '<img ';
+            randomMoviePoster += 'src="' + data.Poster + '" ';
+            randomMoviePoster += 'alt="' + data.Title + '" >';
+            randomMovieHTML += '</div>';
+            randomMovieHTML += '<h1>' + data.Title + "</h1>";
+            randomMovieHTML += '<li>' + data.Rated + '</li>';
+            randomMovieHTML += '<li>' + data.Genre + "</li>";
+            randomMovieHTML += '<li>' + data.Runtime + "</li>";
+            randomMovieHTML += '<button type="button" onclick="refreshPage()">Lookup another movie</button>';
+            randomMovieHTML += '<li>' + data.Plot + "</li>";
+
+            $('#poster-results').html(randomMoviePoster);
+            $('#imdb-results').html(randomMovieHTML);
+        }
+    $.getJSON(randomMovieURL, randomMovieOptions, displayRandomMovies);
+    });
+});
